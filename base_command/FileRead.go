5a8091fd0af22aa4c51200f522f668aaa7c38815
zongzhou.fang <zongzhou.fang@wormpex.com>
2018-08-10 01:41:17 +0800

    wojiufule

--- base_command/FileRead.go
+++ base_command/FileRead.go
@@ -1,10 +1,12 @@
-package basecommand
+package main
+
 import (
-"fmt"
-"io/ioutil"
-"os"
-"strings"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
+
 func main() {
 	counts := make(map[string]int)
 	for _, filename := range os.Args[1:] {
@@ -12,10 +14,12 @@ func main() {
 		fmt.Print(string(data))
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
-			continue }
+			continue
+		}
 		for _, line := range strings.Split(string(data), "\n") {
 			counts[line]++
-		} }
+		}
+	}
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
